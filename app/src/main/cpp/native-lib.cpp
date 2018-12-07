2eb7f07712b52a916efc1e9f630b8a31c15c246a
xjh <413112417@qq.com>
2018-12-07 17:01:46 +0800

    添加ffmpeg库

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,18 @@
 #include <jni.h>
 #include <string>
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavcodec/jni.h>
+#include <libswscale/swscale.h>
+#include <libswresample/swresample.h>
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_pers_hexuren_inkplayer_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = avformat_configuration();
     return env->NewStringUTF(hello.c_str());
 }
